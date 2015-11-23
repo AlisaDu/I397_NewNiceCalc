aea16d850a9899b47109cb0e5d696ce3f412439d
SmiAlisa <alisa.dudinova@itcollege.ee>
2015-11-24 00:52:03 +0200

    Problem: doesn't perform 2 operations in a row, without intermediate =.  ToDo; Try to redo layout with PercentRelativeLayout

--- app/src/main/java/com/alisa/i397_calc/MainActivity.java
+++ app/src/main/java/com/alisa/i397_calc/MainActivity.java
@@ -10,19 +10,30 @@ import static android.view.View.*;
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText screen;
-    private float numberBefore; //stores number that wqas on screen before operator pressed
-    private String operation;
+    private EditText screen; // displays current inout and results
+    private float operand1; //stores number that was on screen before operator btn pressed
+    private float operand2;
+    private String operation; // hold current operator +-*/
     private ButtonClickListener btnClick;
-    private String lastOperand = "x";
-    private float intermResult = 0; // holds the result of last "equal" operation
-
+    private String lastOperator = "x"; // previousOperator, stored until is replased by another operator +-*/=
+    private float intermResult = 0; // holds the result of last "equal" operation - broken, now. check!
+    private String lastSymbol = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         screen = (EditText) findViewById(R.id.txtResult);
+
+        if (savedInstanceState != null){
+            operand1 =savedInstanceState.getFloat("operand1");
+            operand2 =savedInstanceState.getFloat("operand2");
+            operation = savedInstanceState.getString("operation");
+            lastOperator = savedInstanceState.getString("lastOperator");
+            intermResult = savedInstanceState.getFloat("intermResult");
+            lastSymbol = savedInstanceState.getString ("lastSymbol");
+
+        }
         screen.setText("0");
         btnClick = new ButtonClickListener();
         int idList[] = {R.id.button0, R.id.button1, R.id.button2, R.id.button3, R.id.button4,
@@ -34,100 +45,109 @@ public class MainActivity extends AppCompatActivity {
             View v = (View)findViewById(id);
             v.setOnClickListener(btnClick);
         }
-
-
-
     }
+
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState){
+        super.onSaveInstanceState(savedInstanceState);
+        savedInstanceState.putFloat("operand1", operand1);
+        savedInstanceState.putFloat("operand2", operand2);
+        savedInstanceState.putString("operation", operation);
+        savedInstanceState.putString("lastOperator", lastOperator);
+        savedInstanceState.putFloat("intermResult", intermResult);
+        savedInstanceState.putString ("lastSymbol", lastSymbol);
+    }
+
+
 
     public void mMath(String operator){
 
-        numberBefore = Float.parseFloat (String.valueOf(screen.getText()));
+        operand1 = Float.parseFloat (String.valueOf(screen.getText()));
         operation = operator;
-        screen.setText("0");
+
     }
 
 
     public void getKeyboard(String number){
 
         String currentScreen = screen.getText().toString();
-         if (currentScreen.equals("0")){
+         if (currentScreen.equals("0") || lastSymbol.equals("+")|| lastSymbol.equals("-")|| lastSymbol.equals("*")|| lastSymbol.equals("/")){
              currentScreen = "";
          }
-         if ( lastOperand.equals("=")){
+         if ( lastOperator.equals("=")){
              currentScreen = "";
-             numberBefore = intermResult;
-             lastOperand = "x";
-
+             operand1 = intermResult;
+             lastOperator = "x";
          }
         currentScreen += number;
         screen.setText(currentScreen);
     }
 
+
     public void mResult (){
 
-        float numberAfter =  Float.parseFloat (screen.getText().toString());
+        operand2 =  Float.parseFloat (screen.getText().toString());
         float result =0;
 
+        lastOperator = operation;
         if (operation.equals("+")){
-            result = numberBefore + numberAfter;
-
+            result = operand1 + operand2;
         }
         if (operation.equals("-")){
-            result = numberBefore - numberAfter;
-
+            result = operand1 - operand2;
         }
         if (operation.equals("*")){
-            result = numberBefore * numberAfter;
+            result = operand1 * operand2;
         }
         if (operation.equals("/")){
-            result = numberBefore / numberAfter;
+            result = operand1 / operand2;
         }
+
         intermResult = result;
+        operand1 = result;
+        operand2 = 0;
         screen.setText(String.valueOf(result));
     }
 
 
-
     private class ButtonClickListener implements OnClickListener{
+
         public void onClick (View v){
+
             switch (v.getId()){
                 case R.id.buttonC:
                     screen.setText ("0");
-                    numberBefore = 0;
+                    operand1 = 0;
+                    operand2 = 0;
                     operation = "";
+                    intermResult = 0;
+                    lastOperator = "x";
+                    intermResult = 0;
+                    lastSymbol = "";
                     break;
                 case R.id.buttonSum:
                     mMath ("+");
-                    lastOperand= "+";
                     break;
                 case R.id.buttonSub:
                     mMath ("-");
-                    lastOperand= "-";
                     break;
                 case R.id.buttonMlt:
                     mMath ("*");
-                    lastOperand= "*";
                     break;
                 case R.id.buttonDiv:
                     mMath("/");
-                    lastOperand = "/";
                     break;
                 case R.id.buttonEq:
                     mResult();
-                    lastOperand= "=";
+                    lastOperator = "=";
                     break;
                 default:
                     String number = ((Button) v). getText().toString();
                     getKeyboard(number);
                     break;
-
-
-
             }
+            lastSymbol= ((Button) v). getText().toString();
         }
     }
-
-
 }
-
-
